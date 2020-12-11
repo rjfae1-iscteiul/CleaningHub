76d9020d85426ece87d57e2dbbd86acd57608fef
André Correia <a.correia@accenture.com>
2020-12-11 18:48:34 +0000

    Build prep

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import FirstImg from "../resources/Flexible.jpeg";
 import SecondImg from "../resources/Reliable.jpeg";
