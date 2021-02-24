ce1b7c7cb6552d394dbecc4fb94185025239a505
Ricardo Jorge Ferreira <rjfae1@iscte-iul.pt>
2021-02-24 22:47:31 +0000

    Mapeamento header com login e register

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,4 @@
 import React, { useEffect } from "react";
-import BannerHome from "../../components/Banner";
-import BannerSafety from "../../components/SafetyBanner";
-import BannerOptions from "../../components/OptionBanner";
-import FooterBanner from "../../components/Footer";
-
 export default function Home() {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -11,10 +6,6 @@ export default function Home() {
   
   return (
     <div>
-      <BannerHome />
-      <BannerSafety />
-      <BannerOptions />
-      <FooterBanner />
     </div>
   );
 }
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,8 +1,4 @@
 import React, { useEffect } from "react";
-import BannerHome from "../../components/Banner";
-import BannerSafety from "../../components/SafetyBanner";
-import BannerOptions from "../../components/OptionBanner";
-import FooterBanner from "../../components/Footer";
 
 export default function Home() {
   useEffect(() => {
@@ -11,10 +7,6 @@ export default function Home() {
   
   return (
     <div>
-      <BannerHome />
-      <BannerSafety />
-      <BannerOptions />
-      <FooterBanner />
     </div>
   );
 }
