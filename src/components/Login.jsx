b32271a3b1dc827cd49ced218c1ca1ddb704fa35
fredLiborio <falos@iscte.pt>
2021-03-07 13:46:54 +0000

    added forgot pass login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -15,14 +15,30 @@ class Login extends React.Component
 
     render() 
     {
+      const styleDiv = {
+        paddingTop: "200px",
+        textAlign: "center",
+        fontFamily: "Calibri",
+        paddingLeft: "600px"
+      };
+  
+      const styleTexBox = {
+        height: "24px",
+        fontFamily: "Calibri"
+      }
+      const styleLabel = {
+        fontFamily: "Calibri",
+        fontWeight: "bold"
+      }
         return (
-                <div style={{paddingTop:100}} className="Login">
+                <div style={styleDiv} className="Login">
                   <Form>
 
                   {/* <Form onSubmit={handleSubmit}> */}
                     <Form.Group size="lg" controlId="email">
                       <Form.Label>Email</Form.Label>
                       <Form.Control
+                        style={styleTexBox}
                         autoFocus
                         type="email"
                         // onChange={(e) => setEmail(e.target.value)}
@@ -31,10 +47,13 @@ class Login extends React.Component
                     <Form.Group size="lg" controlId="password">
                       <Form.Label>Password</Form.Label>
                       <Form.Control
+                        style={styleTexBox}
                         type="password"
                         // onChange={(e) => setPassword(e.target.value)}
                       />
                     </Form.Group>
+                   
+			          	  <p><a href="#">Forgot your password?</a></p>
                     <Button block size="lg" type="submit">
                       Login
                     </Button>
